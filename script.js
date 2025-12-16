28ea88ae2a26361a6d11bc23494e717384a5150d
Lokesh Sharma <iamlokesh1406@gmail.com>
2025-12-16 22:06:59 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-let allElems =  document.querySelectorAll(".elem");
+function openFeature() {
+  let allElems =  document.querySelectorAll(".elem");
 let allFullElems = document.querySelectorAll(".fullElem");
+let allFullElemsBackBtn = document.querySelectorAll(".fullElem .back");
 
 allElems.forEach(function(elem){
   elem.addEventListener("click", function(){
-    //  console.log(elem.id);
-
-   allFullElems[elem.id].style.display = "block";
-    
-  })
-    
+   allFullElems[elem.id].style.display = "block"; 
+  })   
 })
+allFullElemsBackBtn.forEach(function(back){
+  back.addEventListener("click", function(){
+   allFullElems[back.id].style.display = "none"; 
+  })   
+})
+}
+// openFeature();
+
+let form = document.querySelector(".addTask form")
+let taskInput = document.querySelector(".addTask form #task-input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #check");
+
+let currTask = [
+  {
+    task: "Mandir jao",
+    details: "Pooja karni hai",
+    imp: true
+  },
+  {
+    task: "Padhayi karo",
+    details: "Career ke liye",
+    imp: true
+  },
+  {
+    task: "Lunch karo",
+    details: "Bhookh lagi hai",
+    imp: false
+  },
+];
+
+function renderTask(){
+ let allTask = document.querySelector(".allTask");
+
+ let sum = "";
+
+ currTask.forEach(function(elem){
+  sum += 
+  `<div class="task">
+        <h5>${elem.task} <span class="${elem.imp ? 'true' : 'false'}">imp</span></h5>
+        <button>Mark as Completed</button>
+        </div>`
+ })
+
+ allTask.innerHTML = sum; 
+}
+renderTask();
+
+form.addEventListener("submit", function(e){
+  e.preventDefault(); 
+
+ currTask.push({task: taskInput.value, details: taskDetailsInput.value, imp: taskCheckbox.checked});
+
+taskInput.value = "";
+taskDetailsInput.value = "";
+taskCheckbox.checked = false;
+
+ renderTask();
+
+})
+
